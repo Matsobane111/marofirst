8dc0aea8a17ac93ffdda311771d331ec55c9d8d1
Sinah Makgato <Sinah.Makgato@dynamicdna.co.za>
2021-05-20 12:40:58 +0200

    mycommit

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace marofirst
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
